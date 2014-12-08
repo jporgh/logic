10a4113c3171199e36d5e3c2dd75fd3ad53abb21
jporgh <jpor@yandex.com>
2014-12-08 08:29:24 +0000

    check for updated input files

--- solve.sh
+++ solve.sh
@@ -7,8 +7,9 @@ solve() {
 	input="$1"
 	tmp="$(basename "$1" .p).tmp"
 	output="$(basename "$1" .p).out"
-	[ -f "$output" ] && return 0
+	[ ! -f "$output" -o "$input" -nt "$output" ] || return 0
 	echo "$input"
+	rm -f "$output"
 	if fgrep -qx '%model' "$input"
 	then
 		tptp_to_ladr < "$input" | mace4 -t$TIMEOUT >"$tmp"
