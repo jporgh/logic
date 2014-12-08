9e29e7389f273216570d5bb5c4d43c6b42dffc19
jporgh <jpor@yandex.com>
2014-12-08 08:15:44 +0000

    keep prover output

--- solve.sh
+++ solve.sh
@@ -1,15 +1,21 @@
 #!/bin/sh
 set -e
 
+TIMEOUT=30
+
 solve() {
 	input="$1"
+	tmp="$(basename "$1" .p).tmp"
+	output="$(basename "$1" .p).out"
+	[ -f "$output" ] && return 0
 	echo "$input"
 	if fgrep -qx '%model' "$input"
 	then
-		tptp_to_ladr < "$input" | mace4 -t30 >/dev/null
+		tptp_to_ladr < "$input" | mace4 -t$TIMEOUT >"$tmp"
 	else
-  		tptp_to_ladr < "$input" | prover9 -t30 >/dev/null
+  		tptp_to_ladr < "$input" | prover9 -t$TIMEOUT >"$tmp"
 	fi
+	mv "$tmp" "$output"
 }
 
 for a in *.p
