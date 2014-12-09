d55494e02c0843774dadf7673607c49af739a363
jporgh <jporgh@users.noreply.github.com>
2014-12-09 15:56:52 +0100

    Create zfc.v

---
+++ models/zfc.v
@@ -0,0 +1,42 @@
+Require Import Bool.
+
+Section Model.
+
+Inductive nat : Set :=
+| O : nat
+| S : nat -> nat.
+
+Fixpoint oddb (n : nat) : bool :=
+match n with
+| O => false
+| S n => negb (oddb n)
+end.
+
+Fixpoint div2 (n : nat) : nat :=
+match n with
+| O => O
+| S O => O
+| S (S n) => S (div2 n)
+end.
+
+Fixpoint elem (n m : nat) {struct n}: bool :=
+match n with
+| O => oddb m
+| S n => elem n (div2 m)
+end.
+
+Lemma O_is_emptyset : forall n, elem n O = false.
+Proof.
+induction n; auto.
+Qed.
+
+Lemma extensionality : forall m n, (forall x, elem x m = elem x n)
+ -> m = n.
+Proof.
+intros m n H.
+induction n.
+destruct m.
+trivial.
+Qed.
+
+End Model.
